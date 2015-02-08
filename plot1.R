577a4207253f49bc447e5d4d85d12000d42ac41e
Antony Yatcheny <antony.yatcheny@yandex.ru>
2015-02-09 00:27:44 +0300

    done

---
+++ plot1.R
@@ -0,0 +1,25 @@
+library(dplyr)
+FIL.txt <- paste(getwd(), "data", "household_power_consumption.txt", sep = "/")
+FIL.tmp <- tempfile()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              destfile = FIL.tmp, method = "curl")
+unzip(zipfile = FIL.tmp, exdir = DIR.dat)
+unlink(FIL.tmp)
+
+epc <- read.csv2(FIL.txt,
+                 col.names = c("dat", "tim", "gap", "grp", "vlt", "gnt", "sm1", "sm2", "sm3")) %>%
+        mutate(dat = as.Date(dat, format = "%m/%d/%Y")) %>%
+        filter(dat == "2007-02-01" | dat == "2007-02-02") %>%
+        mutate(tim = as.POSIXct(paste(as.character(dat), tim), format = "%Y-%m-%d %H:%M:%S"),
+               gap = as.numeric(levels(gap))[gap],
+               grp = as.numeric(levels(grp))[grp],
+               vlt = as.numeric(levels(vlt))[vlt],
+               gnt = as.numeric(levels(gnt))[gnt],
+               sm1 = as.numeric(levels(sm1))[sm1],
+               sm2 = as.numeric(levels(sm2))[sm2],
+               sm3 = as.numeric(levels(sm3))[sm3])
+
+# plot1.png
+png(file = "plot1.png")
+hist(epc$gap, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,32 @@
+library(dplyr)
+
+FIL.txt <- paste(getwd(), "data", "household_power_consumption.txt", sep = "/")
+FIL.tmp <- tempfile()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              destfile = FIL.tmp, method = "curl")
+unzip(zipfile = FIL.tmp, exdir = DIR.dat)
+unlink(FIL.tmp)
+
+epc <- read.csv2(FIL.txt,
+                 col.names = c("dat", "tim", "gap", "grp", "vlt", "gnt", "sm1", "sm2", "sm3")) %>%
+        mutate(dat = as.Date(dat, format = "%m/%d/%Y")) %>%
+        filter(dat == "2007-02-01" | dat == "2007-02-02") %>%
+        mutate(tim = as.POSIXct(paste(as.character(dat), tim), format = "%Y-%m-%d %H:%M:%S"),
+               gap = as.numeric(levels(gap))[gap],
+               grp = as.numeric(levels(grp))[grp],
+               vlt = as.numeric(levels(vlt))[vlt],
+               gnt = as.numeric(levels(gnt))[gnt],
+               sm1 = as.numeric(levels(sm1))[sm1],
+               sm2 = as.numeric(levels(sm2))[sm2],
+               sm3 = as.numeric(levels(sm3))[sm3])
+
+# plot3.png
+attach(epc, warn.conflicts = FALSE)
+png(file = "plot3.png")
+plot (tim, sm3, type = "l", col = "blue", xlab = "", ylab = "Energy sub metering")
+lines(tim, sm2, type = "l", col = "red")
+lines(tim, sm1, type = "l", col = "black")
+legend("topright", lty = 1, col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+detach(epc)
